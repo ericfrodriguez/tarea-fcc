d1284c6e434c1ba4f925c13286517014789008c2
Eric Rodriguez <feric.rodriguez@gmail.com>
2022-12-08 18:34:07 -0300

    Ejercicios ES6 completo

--- ES6.js
+++ ES6.js
@@ -519,16 +519,22 @@ temp = thermos.temperature; // 26 in Celsius
 
 
 
-// 13. 
+// 20. Create a Module Script
 
 
 /*
 
-
+Add a script to the HTML document of type module and give it the source file of index.js
 
 */
 
-
+<html>
+    <body>
+        <!-- Only change code below this line -->
+        <script type="module" src="index.js"></script>
+        <!-- Only change code above this line -->
+    </body>
+</html>
 
 
 
@@ -537,16 +543,24 @@ temp = thermos.temperature; // 26 in Celsius
 
 
 
-// 13. 
+// 21. Use export to Share a Code Block
 
 
 /*
 
-
+There are two string-related functions in the editor. Export both of them using the method of your choice.
 
 */
 
+const uppercaseString = (string) => {
+    return string.toUpperCase();
+}
 
+const lowercaseString = (string) => {
+    return string.toLowerCase()
+}
+
+export { uppercaseString, lowercaseString };
 
 
 
@@ -555,16 +569,20 @@ temp = thermos.temperature; // 26 in Celsius
 
 
 
-// 13. 
+// 22. Reuse JavaScript Code Using import
 
 
 /*
 
-
+Add the appropriate import statement that will allow the current file to use the uppercaseString and lowercaseString functions you exported in the previous lesson. These functions are in a file called string_functions.js, which is in the same directory as the current file.
 
 */
 
+import { uppercaseString, lowercaseString } from './string_functions.js';
+// Only change code above this line
 
+uppercaseString("hello");
+lowercaseString("WORLD!");
 
 
 
@@ -573,16 +591,20 @@ temp = thermos.temperature; // 26 in Celsius
 
 
 
-// 13. 
+// 23. Use * to Import Everything from a File
 
 
 /*
 
-
+The code in this file requires the contents of the file: string_functions.js, that is in the same directory as the current file. Use the import * as syntax to import everything from the file into an object called stringFunctions.
 
 */
 
+import * as stringFunctions from './string_functions.js';
+// Only change code above this line
 
+stringFunctions.uppercaseString("hello");
+stringFunctions.lowercaseString("WORLD!");
 
 
 
@@ -591,16 +613,38 @@ temp = thermos.temperature; // 26 in Celsius
 
 
 
-// 13. 
+// 24. Create an Export Fallback with export default
 
 
 /*
 
-
+The following function should be the fallback value for the module. Please add the necessary code to do so.
 
 */
 
+export default function subtract(x, y) {
+    return x - y;
+}
 
+
+
+// ------------------------------------------------------------------
+
+
+
+// 25. Import a Default Export
+
+
+/*
+
+In the following code, import the default export from the math_functions.js file, found in the same directory as this file. Give the import the name subtract.
+
+*/
+
+import subtract from './math_functions.js';
+// Only change code above this line
+
+subtract(7, 4);
 
 
 
@@ -609,16 +653,44 @@ temp = thermos.temperature; // 26 in Celsius
 
 
 
-// 13. 
+// 26. Create a JavaScript Promise
 
 
 /*
 
-
+Create a new promise called makeServerRequest. Pass in a function with resolve and reject parameters to the constructor.
 
 */
 
+const makeServerRequest = new Promise((resolve, reject) => {
 
+})
+
+
+
+// ------------------------------------------------------------------
+
+
+
+// 27. Complete a Promise with resolve and reject
+
+
+/*
+
+Make the promise handle success and failure. If responseFromServer is true, call the resolve method to successfully complete the promise. Pass resolve a string with the value We got the data. If responseFromServer is false, use the reject method instead and pass it the string: Data not received.
+
+*/
+
+const makeServerRequest = new Promise((resolve, reject) => {
+    // responseFromServer represents a response from a server
+    let responseFromServer;
+
+    if (responseFromServer) {
+        resolve('We got the data');
+    } else {
+        reject('Data not received');
+    }
+});
 
 
 
@@ -627,16 +699,28 @@ temp = thermos.temperature; // 26 in Celsius
 
 
 
-// 13. 
+// 28. Handle a Fulfilled Promise with then
 
 
 /*
 
-
+Add the then method to your promise. Use result as the parameter of its callback function and log result to the console.
 
 */
 
+const makeServerRequest = new Promise((resolve, reject) => {
+    // responseFromServer is set to true to represent a successful response from a server
+    let responseFromServer = true;
 
+    if (responseFromServer) {
+        resolve("We got the data");
+    } else {
+        reject("Data not received");
+    }
+});
+
+makeServerRequest
+    .then(result => console.log(result))
 
 
 
@@ -645,52 +729,28 @@ temp = thermos.temperature; // 26 in Celsius
 
 
 
-// 13. 
+// 29. Handle a Rejected Promise with catch
 
 
 /*
 
-
-
-*/
-
-
-
-
-
-
-// ------------------------------------------------------------------
-
-
-
-// 13. 
-
-
-/*
-
-
+Add the catch method to your promise. Use error as the parameter of its callback function and log error to the console.
 
 */
 
+const makeServerRequest = new Promise((resolve, reject) => {
+    // responseFromServer is set to false to represent an unsuccessful response from a server
+    let responseFromServer = false;
 
+    if (responseFromServer) {
+        resolve("We got the data");
+    } else {
+        reject("Data not received");
+    }
+});
 
-
-
-
-// ------------------------------------------------------------------
-
-
-
-// 13. 
-
-
-/*
-
-
-
-*/
-
-
-
-
-// ------------------------------------------------------------------
+makeServerRequest
+    .then(result => {
+        console.log(result);
+    })
+    .catch(error => console.log(error));
